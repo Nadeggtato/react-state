188f4ffead975c928747fa3858bdd2d8a7e6f641
ia <paepulumbarit@gmail.com>
2025-03-18 19:49:01 -0700

    Add components + style fixing

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,35 @@
+import IngredientsListProps from "@/types/IngredientsListProps"
 import { Inter } from "next/font/google"
 import { useState } from "react"
 
 const inter400 = Inter({ weight: "400", subsets: ['latin'] })
+const inter500 = Inter({ weight: "500", subsets: ['latin'] })
+
+export function IngredientsList(props: IngredientsListProps)
+{
+  return (
+    <>
+      <div id="ingredientListDiv">
+        <h1>Ingredients on hand:</h1>
+        <ul id="ingredientList">
+          {props.ingredients.map((ingredient, index) => {
+            return <li key={index}>{ingredient}</li>
+          })}
+        </ul>
+      </div>
+
+      <div id="ingredientsReadyDiv">
+        <div>
+          <p className={inter500.className}>Ready for a recipe?</p>
+          <p>Generate a recipe from your list of ingredients</p>
+        </div>
+        <div>
+          <button>Get a recipe</button>
+        </div>
+      </div>
+    </>
+  )
+}
 
 export default function Main() {
   const [ingredients, setIngredients] = useState<Array<string>>([])
@@ -24,16 +52,7 @@ export default function Main() {
         </form>
       </div>
 
-      {
-        ingredients.length > 0 && (<div id="ingredientListDiv">
-          <h1>Ingredients on hand:</h1>
-          <ul id="ingredientList">
-            {ingredients.map((ingredient, index) => {
-              return <li key={index}>{ingredient}</li>
-            })}
-          </ul>
-        </div>)
-      }
+      {ingredients.length > 0 && <IngredientsList ingredients={ingredients}/>}
     </main>
   )
 }
