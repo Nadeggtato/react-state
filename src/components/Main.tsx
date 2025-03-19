7cc50adf46545ddcc8acc0b5b04ad55c7f88e126
ia <paepulumbarit@gmail.com>
2025-03-18 19:03:39 -0700

    Use && condition instead of ? :

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,24 +1,19 @@
 import { Inter } from "next/font/google"
-import React from "react"
+import { useState } from "react"
 
 const inter400 = Inter({ weight: "400", subsets: ['latin'] })
 
 export default function Main() {
-  const [ingredients, setIngredients] = React.useState<Array<string>>([])
+  const [ingredients, setIngredients] = useState<Array<string>>([])
 
-  function addIngredient(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-
-    const formData = new FormData(event.currentTarget)
-    console.log(formData.get('ingredient') as string)
-
+  function addIngredient(formData: FormData) {
     setIngredients(prevIngredients => [...prevIngredients, formData.get('ingredient') as string])
   }
 
   return (
     <main className={inter400.className}>
       <div id="inputDiv">
-        <form onSubmit={addIngredient}>
+        <form action={addIngredient}>
           <input type="text"
             id="txtIngredient"
             name="ingredient"
@@ -30,14 +25,14 @@ export default function Main() {
       </div>
 
       {
-        ingredients.length > 0 ? (<div id="ingredientListDiv">
+        ingredients.length > 0 && (<div id="ingredientListDiv">
           <h1>Ingredients on hand:</h1>
           <ul id="ingredientList">
             {ingredients.map((ingredient, index) => {
               return <li key={index}>{ingredient}</li>
             })}
           </ul>
-        </div>) : null
+        </div>)
       }
     </main>
   )
