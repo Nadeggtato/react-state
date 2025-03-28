56188bc3ee23a46a969ce2a5c3adc4c284c564f3
ia <paepulumbarit@gmail.com>
2025-03-28 14:12:43 -0700

    Move IngredientsList and Recipe to a separate component file

---
+++ src/components/IngredientsList.tsx
@@ -0,0 +1,30 @@
+import IngredientsListProps from "@/types/IngredientsListProps"
+import { Inter } from "next/font/google"
+
+const inter500 = Inter({ weight: "500", subsets: ['latin'] })
+
+export default function IngredientsList(props: IngredientsListProps)
+{
+  return (
+    <>
+      <div id="ingredientListDiv">
+        <h1>Ingredients on hand:</h1>
+        <ul id="ingredientList">
+          {props.ingredients.map((ingredient, index) => {
+            return <li key={index}>{ingredient}</li>
+          })}
+        </ul>
+      </div>
+
+      {props.ingredients.length >= 4 && <div id="ingredientsReadyDiv">
+        <div>
+          <p className={inter500.className}>Ready for a recipe?</p>
+          <p>Generate a recipe from your list of ingredients</p>
+        </div>
+        <div>
+          <button onClick={props.onShowRecipe}>Get a recipe</button>
+        </div>
+      </div>}
+    </>
+  )
+}
