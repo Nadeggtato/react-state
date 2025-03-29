7f1ef4d05eecc55323d86fad082717284d114def
ia <paepulumbarit@gmail.com>
2025-03-29 10:54:55 -0700

    Replace dangerouslySetInnerHtml with react-markdown

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -1,12 +1,12 @@
-import RecipeProps from "@/types/RecipeProps";
+import RecipeProps from "@/types/RecipeProps"
+import ReactMarkdown from "react-markdown"
 
 export default function Recipe(props: RecipeProps) {
   return (
     <section>
       <h2 id="chefClaudeHeader">Chef Claude Recommends:</h2>
-      <article className="suggested-recipe-container"
-        aria-live="polite"
-        dangerouslySetInnerHTML={{ __html: props.recipe }}>
+      <article className="suggested-recipe-container" aria-live="polite">
+        <ReactMarkdown children={props.recipe}></ReactMarkdown>
       </article>
     </section>
   )
